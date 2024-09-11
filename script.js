f8003e9472d203c5bf86b72b28eb2d3cc9204743
ug <subrahmanyaug@gmail.com>
2024-09-11 10:45:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const input=document.getElementById("input-city");
+const button=document.getElementById("button-search");
+const display=document.getElementById("weather-display");
+button.addEventListener("click",async ()=>{
+    const val=input.value.trim();
+    if(!val){
+        alert("enter city name");
+        return;
+    }
+    try{
+       const weather_response=await fetch(`http://api.weatherapi.com/v1/current.json?key=a2d030a4419048238f8153001241009&q=${val}&aqi=no`);
+       if(!weather_response.ok)
+       {
+        throw new Error(weather_response.status)
+       }
+       const weather_report=await weather_response.json();
+       display.innerHTML=`<h1>Location - ${weather_report.location.name}</h1><hr/>
+       <h2>Region - ${weather_report.location.region}</h2>
+       <h2>Country - ${weather_report.location.country}</h2>
+       <h2>Local time - ${weather_report.location.localtime}</h2>
+       <h3>Temperature in celsius - ${weather_report.current.temp_c}*c</h3>
+       <h3>Temperature in fahrenheit - ${weather_report.current.temp_f}f</h3>
+       <h4>Last update - ${weather_report.current.last_updated}</h4>
+       <h1>--- ${weather_report.current.condition.text} ---</h1>
+       <img width="150px"height="150px"src="${weather_report.current.condition.icon}"/>`
+       ;
+    }
+    catch(error){
+        alert(error.message);
+        return;
+    }
+})
+
